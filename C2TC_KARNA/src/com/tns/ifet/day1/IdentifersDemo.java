794fc29867701db66fd01f1d059dff5dbfed024d
karnaleee <karnasankar8746@gmail.com>
2024-10-25 15:27:29 +0530

    DAY1 AND 2

---
+++ C2TC_KARNA/src/com/tns/ifet/day1/IdentifersDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number); 
+		String studentName="Aniket";
+		System.out.println("value is : "+ studentName);
+	}
+}
