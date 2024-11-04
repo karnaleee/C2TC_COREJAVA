ddda8706ffb29b3c236cc689da05e126592fbba1
karnaleee <karnasankar8746@gmail.com>
2024-11-04 15:49:52 +0530

    day3 finished

---
+++ C2TC_KARNA/src/com/tns/ifet/day3/enscapulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.day3.enscapulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		//object creation  
+		OopsConceptDemo obj = new 
+		OopsConceptDemo(); 
+		obj.setSerialNum(101); 
+		obj.setName("Pooja"); 
+		obj.setAge(20); 
+		System.out.println(obj); 
+		} 
+
+	}
+
