14310d0c058ae82aa8ec2b85619af74442277780
karnaleee <karnasankar8746@gmail.com>
2024-11-06 11:58:24 +0530

    day4 finish

---
+++ C2TC_KARNA/src/com/tns/day4/staticvariable/EmployeeDemo.java
@@ -0,0 +1,15 @@
+package com.tns.day4.staticvariable;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e = new Employee("Shubh", 123);
+		System.out.println(e);
+		
+		// Similarly, create the second object of the class and pass the two arguments.
+		e = new Employee("Deep", 321);
+		System.out.println(e);
+
+	}
+
+}
---
+++ C2TC_KARNA/src/com/tns/ifet/day4/usingfinal/FinalMethodClassTest.java
@@ -0,0 +1,5 @@
+package com.tns.ifet.day4.usingfinal;
+
+public class FinalMethodClassTest extends FinalMethodClass {
+
+}
