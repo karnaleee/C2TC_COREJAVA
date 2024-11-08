7feee759e9820b6ec1802b266b5c8fb8cb252333
karnaleee <karnasankar8746@gmail.com>
2024-11-08 11:53:53 +0530

    day6 finished

---
+++ C2TC_KARNA/src/com/tns/ifet/day6/polymorphism/overriding/ICICI.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day6.polymorphism.overriding;
+
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
---
+++ C2TC_KARNA/src/com/tns/ifet/day6/polymorphism/overriding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package com.tns.ifet.day6.polymorphism.overriding;
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
